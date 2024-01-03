95fb8f733305e7d95a338dd55a0f018f1bbd547c
Bilguun.B <zolbooenkhbat@Zolboos-MacBook-Air.local>
2024-01-03 14:38:31 +0800

    update from home

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+export const ContactUs = () => {
+  return (
+    <section className="flex flex-col items-center">
+      <div className="flex flex-col justify-center gap-5 items-center">
+        <div className="flex flex-col w-[624px] gap-5">
+          <div>
+            <p className="text-4xl">Contact Us</p>
+          </div>
+          <div>
+            <p className="text-[#696A75]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam
+            </p>
+          </div>
+        </div>
+        <div className="flex py-2.5 px-0 gap-12 items-center">
+          <div className="flex flex-col gap-2.5 border-2 border-[#E8E8EA] p-4 rounded-xl w-[294px]">
+            <div>
+              <p className="font-bold">Address</p>
+            </div>
+            <div className="w-[260px]">
+              <p className="text-[#696A75]">
+                1328 Oak Ridge Drive, Saint Louis, Missouri
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2.5 border-2 border-[#E8E8EA] p-4 rounded-xl w-[295px]">
+            <div>
+              <p className="font-bold">Contact</p>
+            </div>
+            <div className="w-[160px]">
+              <p className="text-[#696A75]">313-332-8662 info@email.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[663px] bg-[#F6F6F7] justify-center">
+        <div className="flex flex-col items-center pt-[29px] pr-[130px] pb-[26px] pl-[35px] gap-6">
+          <div>
+            <p className="text-xl">Leave a Message</p>
+          </div>
+          <div>
+            <div className="flex gap-3">
+              <div>
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="flex pt-2 pr-2 pb-2 pl-5 w-[225px] h-[38px] border-[1px] border-[#DCDDDF] rounded-md"
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Your email"
+                  className="flex pt-2 pr-2 pb-2 pl-5 w-[225px] h-[38px] border-[1px] border-[#DCDDDF] rounded-md"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import { Navbar } from "@/components/Navbar";
-
+import { ContactUs } from "@/components/ContactUs";
 import { About } from "@/components/About";
 
 export default function Home() {
@@ -9,6 +9,7 @@ export default function Home() {
     <main className="flex w-screen py-8 justify-center">
       <section className="flex flex-col gap-20">
         <Navbar />
+        <ContactUs />
 
         <About />
       </section>

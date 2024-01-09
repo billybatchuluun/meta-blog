dde41b3d51afb2c40894eee85f37952448b8e116
Bilguun.B <zolbooenkhbat@Zolboos-MacBook-Air.local>
2024-01-09 11:59:39 +0800

    update

--- src/components/Cars.jsx
+++
@@ -1,8 +0,0 @@
-function Cars(props) {
-  return (
-    <div>
-      <p>Name: {props.name}</p>
-    </div>
-  );
-}
-export default Cars;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { AllBlogPost } from "@/components/AllBlogPost";
 import { Trending } from "@/components/Trending";
-import { HeroTest } from "@/components/HeroTest";
 import { LoadMore } from "@/components/LoadMore";
 import { About } from "@/components/About";
-
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
